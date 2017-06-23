ebae7b3a0bf1051ce5a1acb3a1299ddc4b13f91d
Sarah Kirk <kirk710@gmail.com>
2017-06-23 16:18:51 -0600

    playing with styling for blog posts

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,25 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create(article_params)
+    if article.save
+      flash[:success] = "Article Successfully Created"
+      redirect_to articles_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
 end
