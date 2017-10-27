8c6a470e516d8d73bc0c60c6d20a2f017c25992b
Sarah Kirk <kirkse710@gmail.com>
2017-10-27 14:06:06 -0600

    updates root path test further, deletes root designation in routes

--- spec/features/a_user_interacts_with_root_path_spec.rb
+++ spec/features/a_user_interacts_with_root_path_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe "A user naviagtes to the root path" do
     visit root_path
     expect(current_path).to eq(about_path)
     expect(page).to have_css(".main-nav")
-    within(.main-nav) do
+    within(".main-nav") do
       expect(page).to have_content("ABOUT")
       expect(page).to have_content("RESUME")
       expect(page).to have_content("WORK")
@@ -12,26 +12,27 @@ RSpec.describe "A user naviagtes to the root path" do
   end
 end
 
-RSpec.describe "a user interacts with social media on landing page"
+RSpec.describe "a user interacts with social media on landing page" do
+
   it "user interacts with linkedin" do
     visit root_path
     expect(current_path).to eq(about_path)
     click_on "LinkedIn"
-    expect(current_path).to eq(https://www.linkedin.com/in/sarahelizabethkirk/)
+    expect(page.current_url).to eq("https://www.linkedin.com/in/sarahelizabethkirk/")
   end
 
   it "user interacts with twitter" do
     visit root_path
     expect(current_path).to eq(about_path)
     click_on "Twitter"
-    expect(current_path).to eq(https://twitter.com/sarahdactyl71)
+    expect(page.current_url).to eq("https://twitter.com/sarahdactyl71")
   end
 
   it "user interacts with github" do
     visit root_path
     expect(current_path).to eq(about_path)
     click_on "Github"
-    expect(current_path).to eq(https://github.com/sarahdactyl71)
+    expect(page.current_url).to eq("https://github.com/sarahdactyl71")
   end
 
 end
